8b4bb4efd47bca7530d3d41d379ab5ad45c493e9
feilong <841583918@qq.com>
2017-10-25 00:15:01 +0800

    nav-logo finished

--- src/components/index.vue
+++ src/components/index.vue
@@ -1,5 +1,7 @@
 <template>
-  <h1>导航</h1>
+  <div id="nav-logo">
+    <img src="https://icebear.me/Public/Home/images/logo-y.png" alt="白熊求职">
+  </div>
 </template>
 
 <script>
@@ -13,4 +15,15 @@
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style lang='scss' scoped='' type='text/css'>
+  #nav-logo{
+    width: 100%;
+    height: 74px;
+    background-color: #FFD733;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    img{
+      height: 54px;
+    }
+  }
 </style>
